f9b2cb668cd39b3443ff9f7877cb48ce61f634f1
Marian Gude <mkgude@gmail.com>
2020-07-22 16:47:03 -0500

    updated generateMarkdown.js to pull in appropriate information for README generator

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,13 +1,28 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
-# Description  ${data.description}
-# Installation  ${data.installation}
-# Usage  ${data.usage}
-# License  ${data.license}
-# Contributing  ${data.contribution}
-# Tests  ${data.tests}
-# Questions  ${data.questions}
+  1. [Description](#description)
+  2. [Installation](#installation)
+  3. [Usage](#usage)
+  4. [License](#license)
+  5. [Contributing](#contributing)
+  6. [Tests](#tests)
+  7. [Questions](#questions)
+  ### Description
+  * ${data.description}
+  ### Installation
+  * ${data.installation}
+  ### Usage
+  * ${data.usage}
+  ### License
+  * ${data.license}
+  ### Contributing
+  * ${data.contributing}
+  ### Tests
+  * ${data.tests}
+  #### Questions
+  * [${data.github}](https://github.com/${data.github})
+  * ${data.email}
 `;
 }
 
