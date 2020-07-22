7cb6b36a2f65db0d07dda6f7b43c0bbf5a95f929
Marian Gude <mkgude@gmail.com>
2020-07-22 13:58:41 -0500

    made folder updates

---
+++ Develop/utils/generateMarkdown.js
@@ -0,0 +1,14 @@
+// function to generate markdown for README
+function generateMarkdown(data) {
+  return `# ${data.title}
+# Description  ${data.description}
+# Installation  ${data.installation}
+# Usage  ${data.usage}
+# License  ${data.license}
+# Contributing  ${data.contribution}
+# Tests  ${data.tests}
+# Questions  ${data.questions}
+`;
+}
+
+module.exports = generateMarkdown;
